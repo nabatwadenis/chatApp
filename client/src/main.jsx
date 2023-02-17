dbba7c148ff59ccf7163fe5fbd14565603745549
nabatwadenis <dnabatwa9@gmail.com>
2023-02-17 06:39:52 +0300

    Context API implementation still issues to resolve

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContext } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <BrowserRouter>
-      <App />
+      <AuthContext>
+        <App />
+      </AuthContext>  
     </BrowserRouter>
     
   </React.StrictMode>,
